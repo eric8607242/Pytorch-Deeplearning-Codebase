85f8406602b41d45575bebe261b7966ff3e4db17
eric8607242 <e0928021388@gmail.com>
2022-01-06 18:10:57 +0800

    model weight initialization

--- model/model.py
+++ model/model.py
@@ -12,5 +12,20 @@ class Model(nn.Module):
 
         classes = model_config["classes"]
 
+        self._initialize_weights()
+
     def forward(self, x):
         return y
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, (nn.Linear, nn.Embedding)):
+                m.weight.data.normal_(0, 0.02)
